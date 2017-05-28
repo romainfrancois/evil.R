c17d73d892ee2eaa792cb59eca7c1ee835127f40
Romain Francois <romain@thinkr.fr>
2017-05-28 11:07:10 +0200

    Random `if`

--- evil.R
+++ evil.R
@@ -1,5 +1,11 @@
 # Evil Tricks for R
 
-#' Reverse if
+#' Random `if`
 unlockBinding("if", baseenv() )
-assign( "if", function(condition, true, false){ .Primitive("if")(!condition, true, false) }, baseenv() )
+assign( "if", 
+  function(condition, true, false = NULL){ 
+    .Primitive("if")( rbinom(1, 1, .5) , true, false) 
+  }, 
+  baseenv() 
+)
+
